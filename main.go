b71113beb90b3b35533828b9df230eafb37d9190
Stian Frøystein <stianfroy@gmail.com>
2023-01-27 15:48:42 +0900

    feat(swagger): implement swaggo

--- main.go
+++ main.go
@@ -6,13 +6,21 @@ import (
 	"time"
 
 	v1 "github.com/stianfro/chi-no-wadachi/api/v1"
+	_ "github.com/stianfro/chi-no-wadachi/docs"
 	utils "github.com/stianfro/chi-no-wadachi/utils"
+
+	httpSwagger "github.com/swaggo/http-swagger"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/rs/zerolog/log"
 )
 
+// @title Chi No Wadachi API
+// @description This is a simple API that is used to demonstrate how to use Chi as a web framework.
+// @version 1.0.0
+// @license.name MIT
+// @BasePath /api/v1
 func main() {
 	utils.SetEnv()
 
@@ -21,13 +29,16 @@ func main() {
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 
-	r.Get("/api/v1/healthz", v1.HealthZ)
-
 	server := &http.Server{
 		Addr:              ":" + os.Getenv("PORT"),
 		Handler:           r,
 		ReadHeaderTimeout: 5 * time.Second,
 	}
+
+	r.Get("/swagger/*", httpSwagger.Handler(
+		httpSwagger.URL(server.Addr+"/swagger/doc.json"),
+	))
+	r.Get("/api/v1/healthz", v1.HealthZ)
 
 	err := server.ListenAndServe()
 	if err != nil {
