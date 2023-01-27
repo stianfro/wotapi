945e6ae8ed8ea3f4c6fa7656959c700ddf9dbf38
Stian Frøystein <stianfroy@gmail.com>
2023-01-27 13:43:48 +0900

    feat: serve new health endpoint

--- main.go
+++ main.go
@@ -3,19 +3,36 @@ package main
 import (
 	"net/http"
 	"os"
+	"time"
+
+	v1 "github.com/stianfro/chi-no-wadachi/api/v1"
+	utils "github.com/stianfro/chi-no-wadachi/utils"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	v1 "github.com/stianfro/chi-no-wadachi/api/v1"
+	"github.com/rs/zerolog/log"
 )
 
 func main() {
+	utils.SetEnv()
+
 	r := chi.NewRouter()
 	r.Use(middleware.RequestID)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 
-	r.Get("/", v1.Hello)
+	r.Get("/api/v1/healthz", v1.HealthZ)
 
-	http.ListenAndServe(":"+os.Getenv("PORT"), r)
+	server := &http.Server{
+		Addr:              ":" + os.Getenv("PORT"),
+		Handler:           r,
+		ReadHeaderTimeout: 5 * time.Second,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Error().
+			Err(err).
+			Msg("Failed to start server")
+	}
 }
