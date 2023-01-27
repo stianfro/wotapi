b430bd607ab56724a2faf2af481d070bc325fd12
Stian Frøystein <stianfroy@gmail.com>
2023-01-27 13:43:02 +0900

    feat(api): improve healthcheck endpoint

--- api/v1/healthz.go
+++ api/v1/healthz.go
@@ -1,8 +1,19 @@
 package v1
 
-import "net/http"
+import (
+	"net/http"
 
-// Hello is a simple hello world handler
-func Hello(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello 世界"))
+	"github.com/rs/zerolog/log"
+)
+
+// HealthZ is a simple health check handler that returns 200 OK and is used for Kubernetes liveness probes
+func HealthZ(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+
+	_, err := w.Write([]byte("OK"))
+	if err != nil {
+		log.Error().
+			Err(err).
+			Msg("Failed to write response")
+	}
 }
