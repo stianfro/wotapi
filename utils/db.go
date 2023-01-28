36c948ab1d01f0e41b14ff0a9a4fc976555b80ff
Stian Frøystein <stianfroy@gmail.com>
2023-01-28 16:27:08 +0900

    feat(utils): ensure all tables are present

--- utils/db.go
+++ utils/db.go
@@ -16,12 +16,8 @@ func InitDB() (*sql.DB, error) {
 		return nil, err
 	}
 
-	if _, err := db.Exec("SELECT * FROM manga"); err == nil {
-		// Table already exists
-		return db, nil
-	}
-
-	_, err = db.Exec(`
+	if _, err := db.Exec("SELECT * FROM manga"); err != nil {
+		_, err = db.Exec(`
   	CREATE TABLE manga (
   		id TEXT PRIMARY KEY,
   		title TEXT NOT NULL,
@@ -30,11 +26,50 @@ func InitDB() (*sql.DB, error) {
   		publisher TEXT NOT NULL
   	)
 	`)
-	if err != nil {
-		log.Error().
-			Err(err).
-			Msg("Failed to create manga table")
-		return nil, err
+		if err != nil {
+			log.Error().
+				Err(err).
+				Msg("Failed to create manga table")
+			return nil, err
+		}
+	}
+
+	if _, err := db.Exec("SELECT * FROM mangaVolumes"); err != nil {
+		_, err = db.Exec(`
+	  CREATE TABLE mangaVolumes (
+		  id TEXT PRIMARY KEY,
+			mangaID TEXT NOT NULL,
+			number INTEGER NOT NULL,
+			title TEXT NOT NULL,
+			releaseDate TEXT NOT NULL,
+			isbn TEXT NOT NULL,
+			FOREIGN KEY (mangaID) REFERENCES manga(id)
+			)
+		`)
+		if err != nil {
+			log.Error().
+				Err(err).
+				Msg("Failed to create mangaVolumes table")
+			return nil, err
+		}
+	}
+
+	if _, err := db.Exec("SELECT * FROM mangaChapters"); err != nil {
+		_, err = db.Exec(`
+	  CREATE TABLE mangaChapters (
+		  id TEXT PRIMARY KEY,
+			volumeID TEXT NOT NULL,
+			number INTEGER NOT NULL,
+			title TEXT NOT NULL,
+			FOREIGN KEY (volumeID) REFERENCES mangaVolumes(id)
+			)
+		`)
+		if err != nil {
+			log.Error().
+				Err(err).
+				Msg("Failed to create mangaChapters table")
+			return nil, err
+		}
 	}
 
 	return db, nil
