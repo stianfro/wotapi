9e0d1d9ea80a16057d211fa9c4d7748cb9edf736
Stian Frøystein <stianfroy@gmail.com>
2023-02-01 14:30:23 +0900

    feat(utils): set driver and connection based on environment
    to support postgres

--- utils/db.go
+++ utils/db.go
@@ -2,6 +2,10 @@ package utils
 
 import (
 	"database/sql"
+	"os"
+
+	// Import the postgres driver
+	_ "github.com/lib/pq"
 
 	"github.com/jmoiron/sqlx"
 	"github.com/rs/zerolog/log"
@@ -9,7 +13,10 @@ import (
 
 // InitDB initializes the database
 func InitDB() (*sqlx.DB, error) {
-	conn, err := sql.Open("sqlite3", "db.sqlite3")
+	dbConnectionString := os.Getenv("DB_CONNECTION_STRING")
+	dbDriver := os.Getenv("DB_DRIVER")
+
+	conn, err := sql.Open(dbDriver, dbConnectionString)
 	if err != nil {
 		log.Error().
 			Err(err).
@@ -17,7 +24,7 @@ func InitDB() (*sqlx.DB, error) {
 		return nil, err
 	}
 
-	db := sqlx.NewDb(conn, "sqlite3")
+	db := sqlx.NewDb(conn, dbDriver)
 
 	return db, nil
 }
